b279fcc535e6da2dd40a29929505457ff100a742
Erhan <erhan@bagdemir.com>
2019-06-02 02:00:26 +0200

    package names in import statements fixed.

--- rhino-archetype/src/main/resources/archetype-resources/src/main/java/benchmark/RhinoEntity.java
+++ rhino-archetype/src/main/resources/archetype-resources/src/main/java/benchmark/RhinoEntity.java
@@ -16,15 +16,17 @@
 
 package ${groupId}.benchmark;
 
-import io.ryos.rhino.sdk.users.data.User;
-import io.ryos.rhino.sdk.reporting.Recorder;
 import io.ryos.rhino.sdk.annotations.CleanUp;
 import io.ryos.rhino.sdk.annotations.Feeder;
 import io.ryos.rhino.sdk.annotations.Prepare;
 import io.ryos.rhino.sdk.annotations.Scenario;
 import io.ryos.rhino.sdk.annotations.Simulation;
 import io.ryos.rhino.sdk.annotations.UserFeeder;
-import io.ryos.rhino.sdk.feeders.UUIDFeeder;
+import io.ryos.rhino.sdk.feeders.UUIDProvider;
+import io.ryos.rhino.sdk.reporting.Measurement;
+import io.ryos.rhino.sdk.users.data.User;
+import io.ryos.rhino.sdk.users.repositories.OAuthUserRepositoryFactoryImpl;
+import javax.ws.rs.core.Response.Status;
 
 /**
  * An example for annotated entity of benchmark job.
@@ -32,10 +34,12 @@ import io.ryos.rhino.sdk.feeders.UUIDFeeder;
 @Simulation(name = "helloWorld")
 public class RhinoEntity {
 
-    @UserFeeder(max = 10)
+    private static final long WAIT_FOR = 500L;
+
+    @UserFeeder(max = 10, factory = OAuthUserRepositoryFactoryImpl.class)
     private User user;
 
-    @Feeder(factory = UUIDFeeder.class)
+    @Feeder(factory = UUIDProvider.class)
     private String uuid;
 
     @Prepare
@@ -44,8 +48,18 @@ public class RhinoEntity {
     }
 
     @Scenario(name = "hello")
-    public void run(Recorder recorder) {
-        System.out.println("Hello World! Running test with user:" + user.getUsername());
+    public void testHelloWorld(Measurement measurement) {
+        System.out.println(uuid + " Hello World! Running test with user:" + user.getUsername());
+        waitFor(WAIT_FOR);
+        measurement.measure("Request", Status.OK.toString());
+    }
+
+    private void waitFor(long waitFor) {
+        try {
+            Thread.sleep(waitFor);
+        } catch (InterruptedException e) {
+            //
+        }
     }
 
     @CleanUp
