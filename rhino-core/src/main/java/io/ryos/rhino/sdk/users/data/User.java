e33cc6728d1881bf4a5d701612a0209ffb47f4e0
Erhan <erhan@bagdemir.com>
2019-06-06 23:40:22 +0200

    Minor refactorings

--- rhino-core/src/main/java/io/ryos/rhino/sdk/users/data/User.java
+++ rhino-core/src/main/java/io/ryos/rhino/sdk/users/data/User.java
@@ -17,9 +17,10 @@
 package io.ryos.rhino.sdk.users.data;
 
 /**
- * Representation of a User, that is used in requests against the backend. Every requestFor is
+ * Representation of a User, that is used in requests against the backend. Every request is
  * associated with an user.
- *
+ * <p>
+ *   
  * @author Erhan Bagdemir
  * @since 1.0.0
  */
