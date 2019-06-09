18bab869c0fb92cf72a8495d9b298a837d067ad8
Erhan <erhan@bagdemir.com>
2019-06-09 21:14:22 +0200

    New Http methods added for reactive way.

--- rhino-core/src/main/java/io/ryos/rhino/sdk/specs/HttpSpec.java
+++ rhino-core/src/main/java/io/ryos/rhino/sdk/specs/HttpSpec.java
@@ -1,11 +1,12 @@
 package io.ryos.rhino.sdk.specs;
 
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
 public interface HttpSpec extends Spec {
 
-  enum Method {GET, HEAD, PUT, POST}
+  enum Method {GET, HEAD, PUT, POST, OPTIONS, DELETE, PATCH}
 
   HttpSpec get();
   HttpSpec head();
@@ -15,20 +16,18 @@ public interface HttpSpec extends Spec {
   HttpSpec patch();
   HttpSpec options();
 
-
+  HttpSpec upload(InputStream stream);
   HttpSpec target(String endpoint);
-
-  HttpSpec headers(String name, String ... values);
+  HttpSpec headers(String name, String... values);
   HttpSpec queryParam(String name, String value);
   HttpSpec matrixParams(String name, String value);
 
   // Getters
   Method getMethod();
   String getTarget();
-
+  InputStream getUploadContent();
   Map<String, List<Object>> getHeaders();
   Map<String, String> getQueryParameters();
-
   String getEnclosingSpec();
   String getStepName();
 
