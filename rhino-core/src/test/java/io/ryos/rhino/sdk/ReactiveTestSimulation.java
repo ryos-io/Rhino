f9743861483be622da3e22adb6d3c2481623689c
Erhan <erhan@bagdemir.com>
2019-06-09 21:19:56 +0200

    Simulation contains andThen operation.

--- rhino-core/src/test/java/io/ryos/rhino/sdk/ReactiveTestSimulation.java
+++ rhino-core/src/test/java/io/ryos/rhino/sdk/ReactiveTestSimulation.java
@@ -1,5 +1,6 @@
 package io.ryos.rhino.sdk;
 
+import io.ryos.rhino.sdk.annotations.Influx;
 import io.ryos.rhino.sdk.annotations.Runner;
 import io.ryos.rhino.sdk.annotations.Simulation;
 import io.ryos.rhino.sdk.annotations.TestSpec;
@@ -8,17 +9,27 @@ import io.ryos.rhino.sdk.specs.Spec;
 import java.util.UUID;
 
 @Simulation(name = "Reactive Test")
+@Influx
 @Runner(clazz = ReactiveHttpSimulationRunner.class)
 public class ReactiveTestSimulation {
 
-  private static final String HEALTH_ENDPOINT = "https://cc-api-storage-stage.adobe"
-      + ".io/server-status/health";
+  private static final String HEALTH_ENDPOINT = "https://cc-api-storage-stage.adobe.io/server-status/health";
 
   @TestSpec(name="Spec")
   public Spec healthcheckCallSpec() {
-    return Spec.http("Health Check")
+    return Spec
+        .http("Health Check")
         .target(HEALTH_ENDPOINT)
         .headers("X-Request-Id", "Rhino" + UUID.randomUUID().toString())
-        .get();
+        .get()
+        .andThen((session) -> {
+
+          System.out.println(session.get("response"));
+
+          return Spec
+              .http("Step 2")
+              .target(HEALTH_ENDPOINT)
+              .get();
+        });
   }
 }
