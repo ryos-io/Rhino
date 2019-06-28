59942383ffe2a4595f1721c5021c74754e7db437
Bagdemir <bagdemir@adobe.com>
2019-06-28 17:10:47 +0200

    Javadocs for Throttling.

--- rhino-core/src/main/java/io/ryos/rhino/sdk/annotations/Throttle.java
+++ rhino-core/src/main/java/io/ryos/rhino/sdk/annotations/Throttle.java
@@ -3,9 +3,26 @@ package io.ryos.rhino.sdk.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Annotation is used to limit the number of requests that the framework is allowed to produce
+ * within the duration.
+ * <p>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Throttle {
 
+  /**
+   * Duration in minutes in which the throttling is active.
+   * <p>
+   *
+   * @return Throttling duration.
+   */
   int durationInMins() default 1;
+
+  /**
+   * Max. number of requests.
+   *
+   * @return Maximum number of requests.
+   */
   int numberOfRequests() default 5;
 }
