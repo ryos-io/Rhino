77de3498b303abf67230815ce18342d207122647
Erhan Bağdemir <bagdemir@adobe.com>
2019-06-28 14:29:25 +0200

    1.1.0 reactive upload test (#48)
    * Upload content supplier to avoid the same stream to be used.
    
    * Reactive upload test.
    
    * Test file to upload and ignore test for Travis CI.
    
    * endpoints removed.
    
    * Update UploadLoadTest.java
    
    obsolete line break deleted.

--- rhino-core/src/test/java/io/ryos/rhino/sdk/UploadLoadSimulation.java
+++ rhino-core/src/test/java/io/ryos/rhino/sdk/UploadLoadSimulation.java
@@ -4,19 +4,22 @@ import static io.ryos.rhino.sdk.specs.HttpSpec.from;
 import static io.ryos.rhino.sdk.specs.Spec.http;
 
 import io.ryos.rhino.sdk.annotations.Dsl;
+import io.ryos.rhino.sdk.annotations.Provider;
 import io.ryos.rhino.sdk.annotations.Runner;
 import io.ryos.rhino.sdk.annotations.Simulation;
 import io.ryos.rhino.sdk.annotations.UserProvider;
 import io.ryos.rhino.sdk.annotations.UserRepository;
 import io.ryos.rhino.sdk.dsl.LoadDsl;
 import io.ryos.rhino.sdk.dsl.Start;
+import io.ryos.rhino.sdk.feeders.UUIDProvider;
 import io.ryos.rhino.sdk.runners.ReactiveHttpSimulationRunner;
 import io.ryos.rhino.sdk.users.repositories.OAuthUserRepositoryFactoryImpl;
+import java.io.InputStream;
 
-@Simulation(name = "Reactive Test")
+@Simulation(name = "Reactive Upload Test")
 @Runner(clazz = ReactiveHttpSimulationRunner.class)
 @UserRepository(max = 1, factory = OAuthUserRepositoryFactoryImpl.class)
-public class ReactiveBasicHttpGetSimulation {
+public class UploadLoadSimulation {
 
   private static final String DISCOVERY_ENDPOINT = "https://localhost/files";
   private static final String X_REQUEST_ID = "X-Request-Id";
@@ -25,15 +28,26 @@ public class ReactiveBasicHttpGetSimulation {
   @UserProvider
   private io.ryos.rhino.sdk.feeders.UserProvider userProvider;
 
-  @Dsl(name = "Discovery")
+  @Provider(factory = UUIDProvider.class)
+  private UUIDProvider uuidProvider;
+
+  @Dsl(name = "Upload File")
   public LoadDsl singleTestDsl() {
     return Start
         .spec()
-        .run(http("Discovery")
+        .run(http("text.txt")
             .header(c -> from(X_REQUEST_ID, "Rhino-" + userProvider.take()))
             .header(X_API_KEY, SimulationConfig.getApiKey())
             .auth()
-            .endpoint(DISCOVERY_ENDPOINT)
-            .get());
+            .endpoint((c) -> DISCOVERY_ENDPOINT + uuidProvider.take())
+            .upload(this::getStream)
+            .put());
+  }
+
+  private InputStream getStream() {
+
+    final InputStream resourceAsStream = getClass().getResourceAsStream("/test.txt");
+    assert resourceAsStream != null;
+    return resourceAsStream;
   }
 }
---
+++ rhino-core/src/test/java/io/ryos/rhino/sdk/UploadLoadTest.java
@@ -0,0 +1,16 @@
+package io.ryos.rhino.sdk;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore
+public class UploadLoadTest {
+
+  private static final String SIM_NAME = "Reactive Upload Test";
+  private static final String PROPERTIES_FILE = "classpath:///rhino.properties";
+
+  @Test
+  public void testReactiveBasicHttp() {
+    Simulation.create(PROPERTIES_FILE, SIM_NAME).start();
+  }
+}
