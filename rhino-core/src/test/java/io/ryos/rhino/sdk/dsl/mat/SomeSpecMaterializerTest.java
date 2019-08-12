094b4d7036454c4f964071b7c4a14421adf9d046
Bagdemir <bagdemir@adobe.com>
2019-08-12 17:14:00 +0200

    SomeSpecMaterializerTest added.

---
+++ rhino-core/src/test/java/io/ryos/rhino/sdk/dsl/mat/SomeSpecMaterializerTest.java
@@ -0,0 +1,47 @@
+package io.ryos.rhino.sdk.dsl.mat;
+
+import static io.ryos.rhino.sdk.dsl.specs.Spec.some;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import io.ryos.rhino.sdk.data.UserSessionImpl;
+import io.ryos.rhino.sdk.dsl.specs.SomeSpec;
+import io.ryos.rhino.sdk.runners.EventDispatcher;
+import io.ryos.rhino.sdk.users.data.UserImpl;
+import java.util.UUID;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+public class SomeSpecMaterializerTest {
+
+  @Rule
+  public MockitoRule mockitoRule = MockitoJUnit.rule();
+  @Mock
+  EventDispatcher eventDispatcher;
+
+  @Test
+  public void testMaterialize() {
+
+    var user = new UserSessionImpl(
+        new UserImpl("user", UUID.randomUUID().toString(), "", ""));
+
+    final SomeSpec spec = (SomeSpec) some("test").as((u, m) -> {
+      u.add("test", 1);
+      return u;
+    });
+
+    var materialize = new SomeSpecMaterializer(eventDispatcher)
+        .materialize(spec, user);
+
+    var session = materialize.block();
+
+    assertThat(session, notNullValue());
+    assertThat(session.get("test"), notNullValue());
+    assertThat(session.get("test").isPresent(), equalTo(true));
+    assertThat(session.get("test").get(), equalTo(1));
+  }
+}
