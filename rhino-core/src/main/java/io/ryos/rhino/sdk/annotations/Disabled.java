866e60c6fdd625764ac17e375dbdaedaf5b8ad9b
Erhan <erhan@bagdemir.com>
2019-09-20 14:49:30 +0200

    Javadocs added.

--- rhino-core/src/main/java/io/ryos/rhino/sdk/annotations/Disabled.java
+++ rhino-core/src/main/java/io/ryos/rhino/sdk/annotations/Disabled.java
@@ -5,6 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Disables the scenario or DSL. Disabled DSLs/Scenarios will not be run by runners.
+ *
+ * @author Erhan Bagdemir
+ * @since 1.8.0
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Disabled {
