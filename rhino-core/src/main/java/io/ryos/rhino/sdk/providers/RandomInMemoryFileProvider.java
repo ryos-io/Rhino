095420bc8df0b2c2bc6befc07a941f1544898b6e
Bagdemir <bagdemir@adobe.com>
2020-01-17 15:38:23 +0100

    Random in-memory file provider tests.

--- rhino-core/src/main/java/io/ryos/rhino/sdk/providers/RandomInMemoryFileProvider.java
+++ rhino-core/src/main/java/io/ryos/rhino/sdk/providers/RandomInMemoryFileProvider.java
@@ -10,43 +10,46 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * Provider instance to create random in-memory files which can be used in tests e.g upload files.
+ *
+ * @author Erhan Bagdemir
+ */
 public class RandomInMemoryFileProvider extends AbstractProvider<RandomInMemoryFile> {
 
+  private static final String MIME_TYPES = "mimeTypes";
+  private static final String MAX_SIZE = "maxSize";
+  private static final String NUMBER_OF_FILES = "numberOfFiles";
   private List<String> mimeTypes;
   private int maxSize;
   private int numberOfFiles;
-
+  private Random randomizer = new Random();
   private CyclicIterator<RandomInMemoryFile> fileIterator;
 
   public RandomInMemoryFileProvider() {
     try {
-      this.mimeTypes = getConfig("mimeTypes", asList());
-      this.maxSize = getConfig("maxSize", asInt());
-      this.numberOfFiles = getConfig("numberOfFiles", asInt());
-
+      this.mimeTypes = getConfig(MIME_TYPES, asList());
+      this.maxSize = getConfig(MAX_SIZE, asInt());
+      this.numberOfFiles = getConfig(NUMBER_OF_FILES, asInt());
       init();
     } catch (NotReadyException e) {
-      // Not ready
+      // Not ready, safe to ignore this exception.
     }
   }
 
   public RandomInMemoryFileProvider(List<String> mimeTypes, int maxSize, int numberOfFiles) {
-
     this.maxSize = maxSize;
     this.mimeTypes = mimeTypes;
     this.numberOfFiles = numberOfFiles;
-
     init();
   }
 
   private void init() {
     var cyclicIterator = new CyclicIterator<>(mimeTypes);
-    var random = new Random();
     var inMemoryFiles = Stream
-        .generate(() -> new RandomInMemoryFile(random.nextInt(maxSize), cyclicIterator.next()))
+        .generate(() -> new RandomInMemoryFile(randomizer.nextInt(maxSize), cyclicIterator.next()))
         .limit(numberOfFiles)
         .collect(Collectors.toList());
-
     this.fileIterator = new CyclicIterator<>(inMemoryFiles);
   }
 
--- rhino-core/src/test/java/io/ryos/rhino/sdk/providers/RandomInMemoryFileProviderTest.java
+++ rhino-core/src/test/java/io/ryos/rhino/sdk/providers/RandomInMemoryFileProviderTest.java
@@ -1,14 +1,34 @@
 package io.ryos.rhino.sdk.providers;
 
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import io.ryos.rhino.sdk.SimulationConfig;
+import io.ryos.rhino.sdk.utils.Environment;
+import java.io.IOException;
 import org.junit.Test;
 
 public class RandomInMemoryFileProviderTest {
 
-  private static final int NUMBER_OF_FILES = 2;
-  private static final int MAX_SIZE = 100;
+  @Test(expected = NullPointerException.class)
+  public void testRandomInMemoryFileProviderWithMissingConfigs() {
+    SimulationConfig
+        .newInstance("classpath:///rhino_RandomInMemoryFileProvider_missingConfigs.properties",
+            Environment.DEV);
+    new RandomInMemoryFileProvider();
+  }
 
   @Test
-  public void testCreateInMemoryFileList() {
-
+  public void testRandomInMemoryFileProvider() throws IOException {
+    SimulationConfig
+        .newInstance("classpath:///rhino_RandomInMemoryFileProvider.properties", Environment.DEV);
+    final RandomInMemoryFileProvider provider = new RandomInMemoryFileProvider();
+    final RandomInMemoryFile file = provider.take();
+    final String mimeType = file.getMimeType();
+    assertThat(mimeType, equalTo("plain/text"));
+    final byte[] bytes = file.asStream().readAllBytes();
+    assertTrue(bytes.length > 0);
+    assertTrue(bytes.length < 10);
   }
 }
