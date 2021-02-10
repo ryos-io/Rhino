ac0f08230bae69ce6cc4f5c0d8285759e1362653
Mustafa Caylak <mustafa.caylak@web.de>
2021-02-10 20:31:04 +0100

    added monitor test

--- rhino-kotlin/src/main/kotlin/client/Monitor.kt
+++ rhino-kotlin/src/main/kotlin/client/Monitor.kt
@@ -17,15 +17,16 @@ import kotlin.time.seconds
 
 private val LOG = LoggerFactory.getLogger(Monitor::class.java)
 
+data class Status(val rps: Int, val totalRequests: Int, val clock: Duration)
+
 class Monitor(scope: CoroutineScope, interval: Duration = 1000.milliseconds) : AutoCloseable {
     private var start: Instant = Instant.MIN
     private var currentTotalRequests = 0
     private var currentDuration = 0.seconds
-    private var _status = Triple(0, currentTotalRequests, 0.seconds)
+    private var _status = Status(0, currentTotalRequests, 0.seconds)
 
     private val eventConsumer: SendChannel<Event> = scope.actor(capacity = 1000) {
         consumeEach {
-            LOG.debug("Got event: $it")
             when (it) {
                 is RequestSent -> {
                     if (start == Instant.MIN) {
@@ -40,16 +41,18 @@ class Monitor(scope: CoroutineScope, interval: Duration = 1000.milliseconds) : A
     private val monitorJob =
         scope.launch {
             while (coroutineContext.isActive) {
-                val lastTotalRequests = _status.second
-                val rps = (currentTotalRequests - lastTotalRequests)
-                _status = Triple(rps, currentTotalRequests, currentDuration)
-                LOG.debug("Calculated status: $_status")
-                currentDuration += 1.seconds
+                // TODO calc real delay
                 delay(interval)
+                currentDuration += interval
+                val lastTotalRequests = _status.totalRequests
+                val requestsPerInterval = (currentTotalRequests - lastTotalRequests)
+                val rps = ((requestsPerInterval / interval.inMilliseconds) * 1000).toInt()
+                _status = Status(rps, currentTotalRequests, currentDuration)
+                LOG.info("$_status")
             }
         }
 
-    val status: Triple<Int, Int, Duration>
+    val status: Status
         get() = _status
 
     suspend fun send(e: Event) {
--- rhino-kotlin/src/test/kotlin/client/MonitorTest.kt
+++ rhino-kotlin/src/test/kotlin/client/MonitorTest.kt
@@ -1,33 +1,40 @@
 package client
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import client.model.Request
 import client.model.RequestSent
-import kotlinx.coroutines.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
+import kotlin.time.milliseconds
 
 private val LOG = LoggerFactory.getLogger(MonitorTest::class.java)
 
 class MonitorTest {
     @Test
     fun `test monitoring by simulating 1rps`(): Unit = runBlocking {
-        withTimeout(10000) {
-            val monitor = Monitor(CoroutineScope(Dispatchers.IO))
+        // init logger otherwise logging may be broken due to parallel calls
+        LOG.debug("started...")
+        withTimeout(1500) {
+            val interval = 100.milliseconds
+            val monitor = Monitor(this, interval = interval)
             var lastCount = 0
             repeat(10) {
                 val i = it + 1
                 repeat(i) {
                     monitor.send(RequestSent(Request()))
                 }
-//                assertThat(monitor.status).isEqualTo(
-//                    Triple(i, lastCount, it.seconds)
-//                )
-                delay(1000)
-                LOG.debug("status=${monitor.status}")
+                val expectedCount = lastCount + i
+                assertThat(monitor.status).isEqualTo(
+                    Status(it * 10, lastCount, (it * interval.inMilliseconds).milliseconds)
+                )
+                lastCount = expectedCount
+                delay(110) // give it a bit time to calculate
             }
-//        // exceptions (e.g. for failed assertions) won't reach the event consumer
-//        // in the monitor since the time is virtualized
-//        // need to close evetruerything ourselves
+            monitor.close()
         }
     }
 }
